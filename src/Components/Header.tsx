948f82d5f59fe06211aedaf16441813346f5b528
mancinilucas <mancini.lucasg@gmail.com>
2022-06-25 09:45:41 -0300

    feat: header, about, services

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header(){
   return(
     <header className={styles.header}>
-      <a className={styles.logo} href="#">CORTINANDO</a>
+      <a className={styles.logo} href="#">Cortinando</a>
     </header>
   )
 }
